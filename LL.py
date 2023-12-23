769166d5c5ac4aa2a424da46b064415f1a943bcd
Karan Singh Sodhi <karansinghsodhi@Karans-MacBook-Air.local>
2023-12-23 16:36:11 -0500

    LL reversed

--- LL.py
+++ LL.py
@@ -44,22 +44,120 @@ class LL:
                 return popped
             else:
                 temp = temp.next
-#  head                 tail
-#   4      55    11      23
-#  head         tail
-#   4      55    11
+
+    def prependToLL(self, value):
+        newNode = Node(value)
+        if self.count == 0:
+            print("\numty LL")
+            self.head = newNode
+            self.tail = newNode
+        else:
+            newNode.next = self.head
+            self.head = newNode
+        self.count += 1
+        return True
+    
+    def popFirst(self):
+        if self.count == 0:
+            return None
+        temp = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.count -= 1
+        if self.count == 0:
+            self.tail = None
+        return temp
+
+    def getNode(self, index):
+        if index < 0 or index >= self.count: #covers any -integer and out of bound and also when accessing 0 for empty LL
+            return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+    def setValue(self, index, value):
+        temp = self.getNode(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+    def insertNode(self, index, value):
+        newNode = Node(value)
+        if index < 0 or index > self.count:
+            return False
+        elif index == 0:
+            temp = self.head
+            self.head = newNode
+            newNode.next = temp
+            return True
+        preIndex = index - 1
+        temp = self.head
+        for _ in range(preIndex):
+            temp = temp.next
+        temp2 = temp.next
+        temp.next = newNode
+        newNode.next = temp2
+        return True
+
+    def reverseLL(self):
+        if self.count == 0:
+            print("empty LL, no reverse possible")
+        elif self.count == 1:
+            print("Only 1 node, no reverse possible")
+        else:
+            prev = None
+            current = self.head
+            while(current):
+                if prev == None:
+                    self.tail = current
+                if current.next == None:
+                    self.head = current
+                temp = current.next
+                current.next = prev
+                prev = current
+                current = temp
 
 ll = LL(4)
 ll.appendToLL(55)
 ll.appendToLL(11)
 ll.appendToLL(23)
 ll.printLL()
+ll.reverseLL()
 print("")
-print("popped",ll.popFromLL().value)
-print("after popped: ")
 ll.printLL()
-print("")
-print(ll.tail.value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#node2 = ll.getNode(0)
+#print(node2.value if node2 != None else "Index out of bound" )
+#print("popped",ll.popFromLL().value)
+#print("after popped: ")
+#ll.printLL()
+#print("")
+#print(ll.tail.value)
 # print("")
 # ll.popFromLL()
 # ll.printLL()
